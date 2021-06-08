fece4df243f396e901265e0bc14a8cea3520c88b
Ahmad Ali <AhmadAli137@users.noreply.github.com>
2021-06-08 10:12:55 -0400

    first commit: recycler view complete

---
+++ app/src/main/java/com/riis/etaDetroitkotlin/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.riis.etaDetroitkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    //creating a new activity using whatever data is stored in savedInstanceState
+    //...and setting the content to be displayed in activity_main.xml
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //asking the FragmentManager for the current fragment being used in fragment_container from activity_main.xml
+        val currentFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment_container)
+
+        //if there is no current fragment, create a fragment from TransportListFragment.kt
+        if (currentFragment == null) {
+            val fragment = TransportListFragment.newInstance()
+
+            //Create a new fragment transaction, include one add operation in it, and then commit it
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fragment_container, fragment)//adds the fragment to the container
+                .commit()
+        }
+    }
+}
