59cf91bc6a41483962a51393c2831040d2d23850
Mahir <daiansask@gmail.com>
2021-06-10 13:29:55 -0400

    Mahir/Ahmad: fixed navigation menu and recycleview, added route planner frag

--- app/src/main/java/com/riis/etaDetroitkotlin/HomeFragment.kt
+++ app/src/main/java/com/riis/etaDetroitkotlin/HomeFragment.kt
@@ -8,19 +8,27 @@ import android.graphics.Color
 
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.navigation.NavigationView
 import com.riis.etaDetroitkotlin.model.Company
 
 
@@ -29,12 +37,15 @@ import com.riis.etaDetroitkotlin.model.Company
 //It provides the interface for the user to select between different bus companies
 private const val TAG = "HomeFragment"
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment() , NavigationView.OnNavigationItemSelectedListener{
 
     //CLASS VARIABLES
     //---------------
     private lateinit var companyRecyclerView: RecyclerView
     private var adapter: CompanyAdapter? = null
+    private lateinit var navController: NavController
+    private lateinit var drawerMenu: DrawerLayout
+    private lateinit var leListOfCompanies: List<Company>
 
     
     //LINKING FRAGMENT WITH VIEW MODELS
@@ -66,26 +77,66 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        navController = view.findNavController()
+        drawerMenu = requireActivity().findViewById(R.id.drawer_menu)
+        val navView = activity?.findViewById<NavigationView>(R.id.side_nav_view)
+        navView?.setupWithNavController(navController)
+        navView?.setNavigationItemSelectedListener(this)
+
         homeViewModel.companyListLiveData.observe(
             viewLifecycleOwner,
             { companyList ->
                 companyList?.let {
                     Log.d(TAG, "In the observer")
                     updateUI(companyList)
+                    leListOfCompanies = companyList
                 }
             }
         )
     }
 
+
     //UPDATING THE FRAGMENT VIEW
     //--------------------------
     private fun updateUI(companies: List<Company>) {
-        //creating an adapter and connecting it to the model layer data
-        adapter = CompanyAdapter(companies)
-
         //Connecting the RecyclerView to its adapter
-        companyRecyclerView.adapter = adapter
+        companyRecyclerView.adapter = CompanyAdapter(companies)
+    }
 
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        drawerMenu.closeDrawers()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            when (item.itemId) {
+                R.id.nav_route_map -> navController.navigate(R.id.action_home_dest_to_routeMapFragment)
+                R.id.nav_ddot ->{
+                    homeToRoutesSharedViewModel.saveCompany(leListOfCompanies[1])
+                    navController.navigate(R.id.moveToRoutesFragment)
+                }
+                R.id.nav_smart -> {
+                    homeToRoutesSharedViewModel.saveCompany(leListOfCompanies[0])
+                    navController.navigate(R.id.moveToRoutesFragment)
+                }
+                R.id.nav_reflex -> {
+                    homeToRoutesSharedViewModel.saveCompany(leListOfCompanies[2])
+                    navController.navigate(R.id.moveToRoutesFragment)
+                }
+                R.id.nav_people_mover -> {
+                    homeToRoutesSharedViewModel.saveCompany(leListOfCompanies[3])
+                    navController.navigate(R.id.moveToRoutesFragment)
+                }
+                R.id.nav_qline -> {
+                    homeToRoutesSharedViewModel.saveCompany(leListOfCompanies[4])
+                    navController.navigate(R.id.moveToRoutesFragment)
+                }
+                R.id.nav_planner -> {
+                    navController.navigate(R.id.action_home_dest_to_routePlannerFragment)
+                }
+            }
+        }, 500)
+
+        return true
     }
 
     //VIEW HOLDER CLASS FOR RECYCLER VIEW
@@ -118,8 +169,12 @@ class HomeFragment : Fragment() {
         }
 
         override fun onClick(itemView: View) {
-            homeToRoutesSharedViewModel.saveCompany(companyItem)
-            itemView.findNavController().navigate(R.id.moveToRoutesFragment)
+            if (companyItem.name == "Route Map"){
+                navController.navigate(R.id.action_home_dest_to_routeMapFragment)
+            }else{
+                homeToRoutesSharedViewModel.saveCompany(companyItem)
+                itemView.findNavController().navigate(R.id.moveToRoutesFragment)
+            }
         }
     }
 
