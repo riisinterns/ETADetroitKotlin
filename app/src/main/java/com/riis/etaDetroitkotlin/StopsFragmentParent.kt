08eb02bd0407f21dba5c7161a25ae576dd404657
Ahmad Ali <AhmadAli137@users.noreply.github.com>
2021-06-17 14:22:10 -0400

    StopsFragmentChild.kt search bar in progress

--- app/src/main/java/com/riis/etaDetroitkotlin/StopsFragmentParent.kt
+++ app/src/main/java/com/riis/etaDetroitkotlin/StopsFragmentParent.kt
@@ -2,10 +2,10 @@ package com.riis.etaDetroitkotlin
 
 import android.graphics.Color
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.view.inputmethod.EditorInfo
 import android.widget.TextView
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
@@ -18,29 +18,50 @@ import com.riis.etaDetroitkotlin.model.DaysOfOperation
 import com.riis.etaDetroitkotlin.model.RouteStops
 import org.w3c.dom.Text
 
+/*The StopsFragmentParent is a container fragment that holds the StopsFragmentChild fragment. It is able to switch between different
+  ... screens shown by the child fragment corresponding to the following categories: Weekday, Saturday, and Sunday.
+
+  The user can switch between the screens by either swiping across the screen using a viewPager or
+  ... by pressing different tabs using a tab layout.
+*/
+
 class StopsFragmentParent : Fragment() {
 
+    //CLASS VARIABLES
+    //---------------
     private lateinit var days: List<DaysOfOperation>
     private lateinit var tabLayout: TabLayout
     private lateinit var appBarLayout: AppBarLayout
     private lateinit var stopViewPageAdapter: StopViewPageAdapter
     private lateinit var viewPager: ViewPager2
+
+    //links the fragment to a viewModel shared with MainActivity and other fragments
     private val sharedViewModel: SharedViewModel by activityViewModels()
 
+    //CREATING THE FRAGMENT VIEW
+    //--------------------------
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        //inflating the fragment_stops_parent layout as the fragment view
         val view = inflater.inflate(R.layout.fragment_stops_parent, container, false)
+
+        //referencing layout views using their resource ids
         tabLayout = view.findViewById(R.id.tab_layout)
         appBarLayout = view.findViewById(R.id.app_bar_layout)
+
         return view
     }
 
+    //UPDATING THE UI BASED ON NEW MODEL DATA
+    //---------------------------------------
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         viewPager = view.findViewById(R.id.pager)
 
+        //Gett
         sharedViewModel.daysOfOperationLiveData.observe(viewLifecycleOwner, {
             days = it
 
@@ -53,7 +74,9 @@ class StopsFragmentParent : Fragment() {
 
     }
 
-    private fun updateUI(routeStops: List<RouteStops>) {
+
+
+        private fun updateUI(routeStops: List<RouteStops>) {
 
         //set tab menu color
         appBarLayout.setBackgroundColor(Color.parseColor(sharedViewModel.currentCompany?.brandColor))
