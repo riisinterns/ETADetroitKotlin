a815c8c837e902a36a9495d2d10c05b31bf84157
Ahmad Ali <AhmadAli137@users.noreply.github.com>
2021-06-22 17:06:34 -0400

    fixed RouteLoadingDialog.kt memory leak

--- app/src/main/java/com/riis/etaDetroitkotlin/fragment/RouteLoadingDialog.kt
+++ app/src/main/java/com/riis/etaDetroitkotlin/fragment/RouteLoadingDialog.kt
@@ -2,6 +2,7 @@ package com.riis.etaDetroitkotlin.fragment
 
 import android.app.Activity
 import android.app.Dialog
+import android.content.Context
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
@@ -10,43 +11,56 @@ import android.view.LayoutInflater
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
-import androidx.navigation.fragment.navArgs
 import com.riis.etaDetroitkotlin.R
+import com.riis.etaDetroitkotlin.databinding.LoadingDialogBinding
+import java.util.*
 
+private const val ARG_ROUTE = "route"
 
-class RouteLoadingDialog: DialogFragment() {
+class RouteLoadingDialog(val route:String): DialogFragment() {
 
-    //CLASS VARIABLES
-    //---------------
-    private val safeArgs: RouteLoadingDialogArgs by navArgs() //gaining type-safe access to the arguments sent to TimePickerFragment.kt
-    private lateinit var routeText: TextView
+    private var _binding: LoadingDialogBinding? = null
+    private val binding get() = _binding!!
+
 
     //CREATING THE DIALOG
     //---------------------
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-
-        val route = safeArgs.route
+        _binding = LoadingDialogBinding.inflate(LayoutInflater.from(context))
 
         val builder = AlertDialog.Builder(requireActivity())
-
-        val inflater: LayoutInflater? = activity?.layoutInflater
-
-        val myView = inflater?.inflate(R.layout.loading_dialog, null)
-
-        builder.setView(myView)
+        builder.setView(binding.root)
         builder.setCancelable(false)
+
+
         val dialog = builder.create()
 
-        if (myView != null) {
-            routeText = myView.findViewById(R.id.textView)
-        }
-
-        routeText.text = "Loading $route..."
+        binding.textView.text = "Loading $route..."
 
         dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
 
         return dialog
     }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    /*
+    companion object {
+        fun newInstance(route: String): RouteLoadingDialog {
+            val args = Bundle().apply {
+                putSerializable(ARG_ROUTE, route)
+            }
+            return RouteLoadingDialog(route).apply {
+                arguments = args
+            }
+        }
+    }
+
+
+     */
 
 
 }
