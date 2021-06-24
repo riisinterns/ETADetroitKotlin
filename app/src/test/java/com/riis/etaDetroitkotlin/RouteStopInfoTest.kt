e8206d904a33474a8e78a7fcaf57f0bcab249dc2
Wahid Bawa <wahidbawa10@gmail.com>
2021-06-24 15:24:18 -0400

    fixed the error in routes unit test, working on routes stop info unit test

--- app/src/test/java/com/riis/etaDetroitkotlin/RouteStopInfoTest.kt
+++ app/src/test/java/com/riis/etaDetroitkotlin/RouteStopInfoTest.kt
@@ -8,7 +8,7 @@ import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.riis.etaDetroitkotlin.database.BusDao
 import com.riis.etaDetroitkotlin.database.BusDatabase
-import com.riis.etaDetroitkotlin.model.Company
+import com.riis.etaDetroitkotlin.model.RouteStopInfo
 import com.riis.etaDetroitkotlin.model.Routes
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.MatcherAssert.assertThat
@@ -22,7 +22,7 @@ import java.io.IOException
 
 @RunWith(AndroidJUnit4::class)
 @Config(manifest=Config.NONE)
-class RoutesTest {
+class RouteStopInfoTest {
     private lateinit var busDao: BusDao
     private lateinit var db: BusDatabase
 
@@ -46,18 +46,12 @@ class RoutesTest {
     @Test
     @Throws(Exception::class)
     fun writeRouteAndReadList() {
-        val company = Company(2, "DDOT", "#054839", "ddot_bus")
-        val route = Routes(53, 1, company.id, "VERNOR", "Rosa Parks Transit Center to Michgan & Schaefer")
-        busDao.addCompany(company)
-        busDao.addRoute(route)
-        val routes: LiveData<List<Routes>> = busDao.getRoutes(route.companyId)
-
-        routes.observeForever { r ->
-            assertThat(r[0].id, `is`(53))
-            assertThat(r[0].number, `is`(1))
-            assertThat(r[0].companyId, `is`(2))
-            assertThat(r[0].name, `is`("VERNOR"))
-            assertThat(r[0].description, `is`("Rosa Parks Transit Center to Michgan & Schaefer"))
-        }
+//        val route = Routes(53, 1, 2, "VERNOR", "Rosa Parks Transit Center to Michgan & Schaefer")
+//        busDao.addRoute(route)
+//        val routeStopInfo: LiveData<List<RouteStopInfo>> = busDao.getStopsInfoOnRoute(route.id)
+//
+//        routeStopInfo.observeForever { r ->
+//            assertThat(r[0].routeId, `is`(53))
+//        }
     }
 }
