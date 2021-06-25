439d2059a8b2c12fb634bce865989001f8ec04eb
Wahid Bawa <wahidbawa10@gmail.com>
2021-06-25 15:41:30 -0400

    got rid of ?, added sleeps after each thread so as to not cause errors

--- app/src/test/java/com/riis/etaDetroitkotlin/viewmodel/SharedViewModelTest.kt
+++ app/src/test/java/com/riis/etaDetroitkotlin/viewmodel/SharedViewModelTest.kt
@@ -27,9 +27,9 @@ import java.util.*
 @Config(manifest = Config.NONE)
 class SharedViewModelTest : TestCase() {
 
-    private var busDao: BusDao? = null
-    private var db: BusDatabase? = null
-    private var viewModel: SharedViewModel? = null
+    private lateinit var busDao: BusDao
+    private lateinit var db: BusDatabase
+    private lateinit var viewModel: SharedViewModel
 
     @Before
     fun createDb() {
@@ -37,30 +37,26 @@ class SharedViewModelTest : TestCase() {
         db = Room.inMemoryDatabaseBuilder(
             context, BusDatabase::class.java
         ).allowMainThreadQueries().build()
-        busDao = db?.busDao()
+        busDao = db.busDao()
 
         BusRepository.initialize(context)
-        BusRepository.get().setDatabase(db!!)
-        BusRepository.get().setBusDao(busDao!!)
+        BusRepository.get().setDatabase(db)
+        BusRepository.get().setBusDao(busDao)
 
         viewModel = SharedViewModel()
-        print("HERE")
     }
 
     @After
     @Throws(IOException::class)
     fun closeDb() {
-        db?.close()
-        db = null
-        busDao = null
-        viewModel = null
+        db.close()
     }
 
     @Test
     fun testGetCompanyListLiveData() {
-        Thread.sleep(1000)
+
         assertEquals(4, 2+2)
-        Thread.sleep(1000)
+        Thread.sleep(100)
     }
 
     fun testGetRouteListLiveData() {}
@@ -74,7 +70,6 @@ class SharedViewModelTest : TestCase() {
     @Test
     fun testGetArrivalTimes() {
 
-        Thread.sleep(1000)
         val company = Company(2, "DDOT", "#054839", "ddot_bus")
         val route =
             Routes(53, 1, company.id, "VERNOR", "Rosa Parks Transit Center to Michgan & Schaefer")
@@ -85,22 +80,22 @@ class SharedViewModelTest : TestCase() {
         val trip = Trips(2277, 158479010, route.id, direction.id)
         val tripStop = TripStops(trip.id, stop.id, fromString("04:19:11"), 2)
 
-        busDao?.addCompany(company)
-        busDao?.addRoute(route)
-        busDao?.addStop(stop)
-        busDao?.addDaysOfOperation(daysOfOperation)
-        busDao?.addDirection(direction)
-        busDao?.addRouteStop(routeStop)
-        busDao?.addTrips(trip)
-        busDao?.addTripStop(tripStop)
+        busDao.addCompany(company)
+        busDao.addRoute(route)
+        busDao.addStop(stop)
+        busDao.addDaysOfOperation(daysOfOperation)
+        busDao.addDirection(direction)
+        busDao.addRouteStop(routeStop)
+        busDao.addTrips(trip)
+        busDao.addTripStop(tripStop)
 
-        viewModel?.getArrivalTimes(route.id, direction.id, daysOfOperation.id, stop.id)?.observeForever { tripStops ->
+        viewModel.getArrivalTimes(route.id, direction.id, daysOfOperation.id, stop.id).observeForever { tripStops ->
             assertThat(tripStops[0].tripId, `is`(trip.id))
             assertThat(tripStops[0].stopId, `is`(stop.id))
             assertThat(tripStops[0].arrivalTime.toString(), `is`(fromString("04:19:11").toString()))
             assertThat(tripStops[0].stopSequence, `is`(2))
         }
-        Thread.sleep(1000)
+        Thread.sleep(100)
     }
 
     fun testGetCurrentCompany() {}
@@ -109,29 +104,27 @@ class SharedViewModelTest : TestCase() {
 
     @Test
     fun testSaveCompany() {
-        Thread.sleep(1000)
         val company = Company(2, "DDOT", "#054839", "ddot_bus")
-        viewModel?.saveCompany(company)
+        viewModel.saveCompany(company)
 
-        assertThat(viewModel?.currentCompany?.id, `is`(2))
-        assertThat(viewModel?.currentCompany?.name, `is`("DDOT"))
-        assertThat(viewModel?.currentCompany?.brandColor, `is`("#054839"))
-        assertThat(viewModel?.currentCompany?.busImgUrl, `is`("ddot_bus"))
-        Thread.sleep(1000)
+        assertThat(viewModel.currentCompany?.id, `is`(2))
+        assertThat(viewModel.currentCompany?.name, `is`("DDOT"))
+        assertThat(viewModel.currentCompany?.brandColor, `is`("#054839"))
+        assertThat(viewModel.currentCompany?.busImgUrl, `is`("ddot_bus"))
+        Thread.sleep(100)
     }
 
     @Test
     fun testSaveRoute() {
-        Thread.sleep(1000)
         val route = Routes(53, 1, 2, "VERNOR", "Rosa Parks Transit Center to Michgan & Schaefer")
-        viewModel?.saveRoute(route)
+        viewModel.saveRoute(route)
 
-        assertThat(viewModel?.currentRoute?.id, `is`(53))
-        assertThat(viewModel?.currentRoute?.number, `is`(1))
-        assertThat(viewModel?.currentRoute?.companyId, `is`(2))
-        assertThat(viewModel?.currentRoute?.name, `is`("VERNOR"))
-        assertThat(viewModel?.currentRoute?.description, `is`("Rosa Parks Transit Center to Michgan & Schaefer"))
-        Thread.sleep(1000)
+        assertThat(viewModel.currentRoute?.id, `is`(53))
+        assertThat(viewModel.currentRoute?.number, `is`(1))
+        assertThat(viewModel.currentRoute?.companyId, `is`(2))
+        assertThat(viewModel.currentRoute?.name, `is`("VERNOR"))
+        assertThat(viewModel.currentRoute?.description, `is`("Rosa Parks Transit Center to Michgan & Schaefer"))
+        Thread.sleep(100)
     }
 
 
