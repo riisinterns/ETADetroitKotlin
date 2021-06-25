b7a0ec902e5d5e91ec4b6cb8f427abb2885ade1b
Wahid Bawa <wahidbawa10@gmail.com>
2021-06-25 12:49:14 -0400

    cleaned up warnings and added more to string resources

--- app/src/main/java/com/riis/etaDetroitkotlin/ETADetroitApplication.kt
+++ app/src/main/java/com/riis/etaDetroitkotlin/ETADetroitApplication.kt
@@ -3,7 +3,6 @@ package com.riis.etaDetroitkotlin
 import android.app.Application
 import com.riis.etaDetroitkotlin.database.BusRepository
 
-private const val TAG = "ETADetroitApplication"
 
 class ETADetroitApplication : Application() {
     override fun onCreate() {
